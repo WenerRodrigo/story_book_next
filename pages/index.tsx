4c1a85b7a6bf094063fb89a71d15a1f648e5dbbf
Wener Rodrigo <wener19.wr@gmail.com>
2023-09-20 15:25:20 -0300

    iniciando a documentação dos components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '../src/components/Button/Button';
 
 export default function HomeScreen() {
     return (
         <div>
             <h1>Bem vindo</h1>
+            <Button>
+                Botao
+            </Button>
         </div>
     )
 }
